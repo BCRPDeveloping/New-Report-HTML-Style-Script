094d21e391c840f1673f0d9bfffd0b927d872c9f
ZanCehDevelopingSLO <109287095+BCRPDeveloping@users.noreply.github.com>
2022-12-16 17:16:24 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+author '!!Its_Cehic#2006'
+description 'New HTML style Reports'
+version '1.1'
+ui_page 'web/ui.html'
+files {
+	'web/*.*',
+}
+shared_script 'config.lua'
+client_scripts {
+	'client.lua',
+}
+server_scripts {
+	'server.lua',
+}
+
